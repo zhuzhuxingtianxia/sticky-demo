d167f2e5df1bddd32ecc5cad4e551b7e9db96e11
zhuzijia <873391579@qq.com>
2023-02-03 18:16:19 +0800

    Update index.js

--- src/utils/hooks/index.js
+++ src/utils/hooks/index.js
@@ -1,2 +1,8 @@
+/**
+ * @description 自定义hooks
+ * @use Hooks.useEventListener('scroll', function, DOM);
+ * @example Hooks.useEventListener('scroll', ()=> {}, document) //滚动监听
+ * @example useTimeout(()=>{}, 1000) //延时操作
+*/
 export * from './useEventListener';
 export * from './useTimer';
